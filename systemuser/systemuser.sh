f2a0a328392ca82f5dbc3f301d65cdc1c2ac9559
Jessica B. Hamrick <jhamrick@berkeley.edu>
2014-11-18 15:41:18 -0800

    Preserve environment

--- systemuser/systemuser.sh
+++ systemuser/systemuser.sh
@@ -2,7 +2,7 @@
 
 echo "Creating user $USER ($USER_ID)"
 useradd -u $USER_ID -s $SHELL $USER
-sudo -u $USER jupyterhub-singleuser \
+sudo -E -u $USER jupyterhub-singleuser \
   --port=8888 \
   --ip=0.0.0.0 \
   --user=$JPY_USER \
