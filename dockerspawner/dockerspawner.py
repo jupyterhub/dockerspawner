bb8debd0f95fadfa1c60a01e206f2d5161b07393
Richard Frank <rich@quantopian.com>
2015-10-13 16:48:05 -0400

    MAINT: tls_assert_hostname should be either a string or False
    according to urllib3

--- dockerspawner/dockerspawner.py
+++ dockerspawner/dockerspawner.py
@@ -18,8 +18,11 @@ from jupyterhub.spawner import Spawner
 from traitlets import (
     Dict,
     Unicode,
-    Bool
+    Bool,
+    Union,
+    Enum,
 )
+
 
 class DockerSpawner(Spawner):
     
@@ -99,7 +102,9 @@ class DockerSpawner(Spawner):
     tls_ca = Unicode("", config=True, help="Path to CA certificate for docker TLS")
     tls_cert = Unicode("", config=True, help="Path to client certificate for docker TLS")
     tls_key = Unicode("", config=True, help="Path to client key for docker TLS")
-    tls_assert_hostname = Bool(True, config=True, help="If False, do not verify hostname of docker daemon")
+    tls_assert_hostname = Union([Unicode(None, allow_none=True, config=True,
+                                         help="If False, do not verify hostname of docker daemon"),
+                                 Enum([False])])
 
     remove_containers = Bool(False, config=True, help="If True, delete containers after they are stopped.")
     extra_create_kwargs = Dict(config=True, help="Additional args to pass for container create")
