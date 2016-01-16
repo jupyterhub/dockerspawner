d61cbc347890125364fb690fc9b436f933773f6a
Richard Joyce <rjoyce@ucdavis.edu>
2016-01-15 18:27:36 -0800

    Fix systemuser container

--- systemuser/systemuser.sh
+++ systemuser/systemuser.sh
@@ -2,7 +2,7 @@
 
 echo "Creating user $USER ($USER_ID)"
 useradd -u $USER_ID -s $SHELL $USER
-sudo -E -u $USER jupyterhub-singleuser \
+sudo -E PATH="${CONDA_DIR}/bin:$PATH" -u $USER jupyterhub-singleuser \
   --port=8888 \
   --ip=0.0.0.0 \
   --user=$JPY_USER \
