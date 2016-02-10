6a911cc299529c66a342dcafd3913abb58a5ee39
Min RK <benjaminrk@gmail.com>
2016-02-10 16:02:19 +0100

    declare __all__ in __init__
    for pyflakes

--- dockerspawner/__init__.py
+++ dockerspawner/__init__.py
@@ -1,3 +1,5 @@
 from ._version import __version__
 from .dockerspawner import DockerSpawner
 from .systemuserspawner import SystemUserSpawner
+
+__all__ = ['__version__', 'DockerSpawner', 'SystemUserSpawner']
