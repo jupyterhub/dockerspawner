3e896552e3c05d9de69f49afcf31239dde70f3d9
Alec Aivazis <alec@aivazis.com>
2016-03-29 15:22:43 -0700

    added checks for notebook dir in startup scripts

--- singleuser/singleuser.sh
+++ singleuser/singleuser.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 set -e
+
+notebook_arg=""
+if [ -n "${NOTEBOOK_DIR:+x}" ]
+then
+    notebook_arg="--notebook-dir=${NOTEBOOK_DIR}"
+fi
+
 exec jupyterhub-singleuser \
   --port=8888 \
   --ip=0.0.0.0 \
@@ -8,6 +15,5 @@ exec jupyterhub-singleuser \
   --base-url=$JPY_BASE_URL \
   --hub-prefix=$JPY_HUB_PREFIX \
   --hub-api-url=$JPY_HUB_API_URL \
+  ${notebook_arg} \
   $@
-
-    
