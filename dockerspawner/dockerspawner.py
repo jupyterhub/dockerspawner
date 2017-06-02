5769d9144e3877327f005f05f72276b53043f08c
Chris Kees <cekees@gmail.com>
2017-06-02 20:18:28 +0000

    force conversion of port to int

--- dockerspawner/dockerspawner.py
+++ dockerspawner/dockerspawner.py
@@ -493,7 +493,7 @@ class DockerSpawner(Spawner):
             if resp is None:
                 raise RuntimeError("Failed to get port info for %s" % self.container_id)
             ip = resp[0]['HostIp']
-            port = resp[0]['HostPort']
+            port = int(resp[0]['HostPort'])
         return ip, port
 
     def get_network_ip(self, network_settings):
