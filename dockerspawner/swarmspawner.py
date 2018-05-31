450299cf882cef9bae98063ebe7ca948b4d70080
Min RK <benjaminrk@gmail.com>
2018-05-31 15:58:57 +0200

    consolidate swarm/docker
    factor-out methods not in common:
    
    - create_object (docker create, service create)
    - start_object (docker start, no-op)
    - stop_object (docker stop, no-op)
    - remove_object (docker remove, service rm)
    - Fix ip/port resolution for exposed swarm ports

--- dockerspawner/swarmspawner.py
+++ dockerspawner/swarmspawner.py
@@ -10,16 +10,16 @@ from docker.types import (
 )
 from docker.errors import APIError
 from tornado import gen
-from traitlets import Dict, Unicode, Bool, Int, Any, default, observe
+from traitlets import Dict, Unicode, default
 
 from .dockerspawner import DockerSpawner
-import jupyterhub
 
 
 class SwarmSpawner(DockerSpawner):
     """A Spawner for JupyterHub that runs each user's server in a separate docker service"""
 
     object_type = "service"
+    object_id_key = "ID"
 
     @property
     def service_id(self):
@@ -80,6 +80,9 @@ class SwarmSpawner(DockerSpawner):
         ),
     )
 
+    # container-removal cannot be disabled for services
+    remove = True
+
     @property
     def mount_driver_config(self):
         return DriverConfig(
@@ -109,35 +112,42 @@ class SwarmSpawner(DockerSpawner):
         """Check for my id in `docker ps`"""
         service = yield self.get_task()
         if not service:
-            self.log.warn("service not found")
+            self.log.warning("Service %s not found", self.service_name)
             return 0
 
-        service_state = service['Status']
+        service_state = service["Status"]
         self.log.debug(
-            "Service %s status: %s",
-            self.service_id[:7],
-            pformat(service_state),
+            "Service %s status: %s", self.service_id[:7], pformat(service_state)
         )
 
-        if service_state['State'] == 'running':
+        if service_state["State"] in {"running", "pending"}:
             return None
+
         else:
             return pformat(service_state)
 
     @gen.coroutine
     def get_task(self):
         self.log.debug("Getting task of service '%s'", self.service_name)
-        if self.get_service() is None:
+        if self.get_object() is None:
             return None
+
         try:
-            tasks = yield self.docker('tasks', filters={'service': self.service_name, 'desired-state': 'running'})
+            tasks = yield self.docker(
+                "tasks",
+                filters={"service": self.service_name, "desired-state": "running"},
+            )
             if len(tasks) == 0:
                 return None
+
             elif len(tasks) > 1:
-                # self.log.critical(pformat(tasks))
-                raise RuntimeError("Found more than one running notebook task for service '{}'".format(self.service_name))
+                raise RuntimeError(
+                    "Found more than one running notebook task for service '{}'".format(
+                        self.service_name
+                    )
+                )
+
             task = tasks[0]
-            # self.log.critical(pformat(task))
         except APIError as e:
             if e.response.status_code == 404:
                 self.log.info("Task for service '%s' is gone", self.service_name)
@@ -148,101 +158,70 @@ class SwarmSpawner(DockerSpawner):
         return task
 
     @gen.coroutine
-    def start(self):
+    def create_object(self):
         """Start the single-user server in a docker service."""
-        service = yield self.get_object()
-        if service and self.remove:
-            self.log.warning(
-                "Removing service that should have been cleaned up: %s (id: %s)",
-                self.service_name, self.service_id[:7])
-            # remove the service, as well as any associated volumes
-            yield self.docker('remove_service', self.service_id)
-            service = None
+        container_kwargs = dict(
+            image=self.image,
+            env=self.get_env(),
+            args=(yield self.get_command()),
+            mounts=self.mounts,
+        )
+        container_kwargs.update(self.extra_container_spec)
+        container_spec = ContainerSpec(**container_kwargs)
 
-        if service is None:
-            cmd = None
-            if self._user_set_cmd:
-                cmd = self.cmd
+        resources_kwargs = dict(
+            mem_limit=self.mem_limit,
+            mem_reservation=self.mem_guarantee,
+            cpu_limit=int(self.cpu_limit * 1e9) if self.cpu_limit else None,
+            cpu_reservation=int(
+                self.cpu_guarantee * 1e9
+            ) if self.cpu_guarantee else None,
+        )
+        resources_kwargs.update(self.extra_resources_spec)
+        resources_spec = Resources(**resources_kwargs)
 
-            # build the dictionary of keyword arguments for create_service
-            container_kwargs = dict(
-                image=self.image,
-                env=self.get_env(),
-                args=self.get_args(),
-                mounts=self.mounts,
-            )
-            if cmd:
-                container_kwargs['command'] = cmd
-            container_kwargs.update(self.extra_container_spec)
-            container_spec = ContainerSpec(**container_kwargs)
+        task_kwargs = dict(
+            container_spec=container_spec,
+            resources=resources_spec,
+            networks=[self.network_name] if self.network_name else [],
+        )
+        task_kwargs.update(self.extra_task_spec)
+        task_spec = TaskTemplate(**task_kwargs)
 
-            resources_kwargs = dict(
-                mem_limit=self.mem_limit,
-                mem_reservation=self.mem_guarantee,
-                cpu_limit=int(self.cpu_limit * 1e9) if self.cpu_limit else None,
-                cpu_reservation=int(self.cpu_guarantee * 1e9) if self.cpu_guarantee else None,
-            )
-            resources_kwargs.update(self.extra_resources_spec)
-            resources_spec = Resources(**resources_kwargs)
+        endpoint_kwargs = {}
+        if not self.use_internal_ip:
+            endpoint_kwargs["ports"] = {None: (self.port, "tcp")}
+        endpoint_kwargs.update(self.extra_endpoint_spec)
+        endpoint_spec = EndpointSpec(**endpoint_kwargs)
 
-            task_kwargs = dict(
-                container_spec=container_spec,
-                resources=resources_spec,
-                networks=[self.network_name] if self.network_name else [],
-            )
-            task_kwargs.update(self.extra_task_spec)
-            task_spec = TaskTemplate(**task_kwargs)
+        create_kwargs = dict(
+            task_template=task_spec, endpoint_spec=endpoint_spec, name=self.service_name
+        )
+        create_kwargs.update(self.extra_create_kwargs)
 
-            endpoint_kwargs = {}
-            endpoint_kwargs.update(self.extra_endpoint_spec)
-            endpoint_spec = EndpointSpec(**endpoint_kwargs)
+        return (yield self.docker("create_service", **create_kwargs))
 
-            create_kwargs = dict(
-                task_template=task_spec,
-                endpoint_spec=endpoint_spec,
-                name=self.service_name,
-            )
-            create_kwargs.update(self.extra_create_kwargs)
+    @gen.coroutine
+    def remove_object(self):
+        self.log.info("Removing %s %s", self.object_type, self.object_id)
+        # remove the container, as well as any associated volumes
+        yield self.docker("remove_" + self.object_type, self.object_id)
 
-            resp = yield self.docker('create_service', **create_kwargs)
+    @gen.coroutine
+    def start_object(self):
+        """Nothing to do here
 
-            self.object_id = resp['Id']
-            self.log.info(
-                "Created service '%s' (id: %s) from image %s",
-                self.service_name, self.service_id[:7], self.image)
+        There is no separate start action for services
+        """
+        pass
 
-        else:
-            self.log.info(
-                "Found existing service '%s' (id: %s)",
-                self.service_name, self.service_id[:7])
-            # Handle re-using API token.
-            # Get the API token from the environment variables
-            # of the running service:
-            for line in service['Config']['Env']:
-                if line.startswith(('JPY_API_TOKEN=', 'JUPYTERHUB_API_TOKEN=')):
-                    self.api_token = line.split('=', 1)[1]
-                    break
+    @gen.coroutine
+    def stop_object(self):
+        """Nothing to do here
 
-        # TODO: handle unpause
-        self.log.info(
-            "Starting service '%s' (id: %s)",
-            self.service_name, self.service_id[:7])
-
-        # build the dictionary of keyword arguments for start
-        start_kwargs = {}
-        start_kwargs.update(self.extra_start_kwargs)
-
-        # start the service
-        # TODO: service equivalent for this?
-        # yield self.docker('start', self.service_id, **start_kwargs)
-
-        ip, port = yield self.get_ip_and_port()
-        if jupyterhub.version_info < (0, 7):
-            # store on user for pre-jupyterhub-0.7:
-            self.user.server.ip = ip
-            self.user.server.port = port
-        # jupyterhub 0.7 prefers returning ip, port:
-        return (ip, port)
+        There is no separate stop action for services
+        """
+        pass
 
     @gen.coroutine
     def get_ip_and_port(self):
@@ -259,27 +238,26 @@ class SwarmSpawner(DockerSpawner):
         are correct, which depends on the route to the service
         and the port it opens.
         """
-        ip = self.service_name
-        port = self.port
+        if self.use_internal_ip:
+            ip = self.service_name
+            port = self.port
+        else:
+            # discover published ip, port
+            ip = self.host_ip
+            service = yield self.get_object()
+            for port_config in service["Endpoint"]["Ports"]:
+                if port_config.get("TargetPort") == self.port:
+                    port = port_config["PublishedPort"]
+                    break
+
+            else:
+                self.log.error(
+                    "Couldn't find PublishedPort for %s in %s",
+                    self.port,
+                    service["Endpoint"]["Ports"],
+                )
+                raise RuntimeError(
+                    "Couldn't identify port for service %s", self.service_name
+                )
 
         return ip, port
-
-    @gen.coroutine
-    def stop(self, now=False):
-        """Stop the service
-
-        Consider using pause/unpause when docker-py adds support
-        """
-        self.log.info(
-            "Stopping service %s (id: %s)",
-            self.service_name, self.service_id[:7])
-        yield self.docker('remove_service', self.service_id)
-
-        # if self.remove:
-        #     self.log.info(
-        #         "Removing service %s (id: %s)",
-        #         self.service_name, self.service_id[:7])
-        #     # remove the service, as well as any associated volumes
-        #     yield self.docker('remove_service', self.service_id, v=True)
-
-        self.clear_state()
