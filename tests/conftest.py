6e818d57076bb23b48d50af30224922cc1787116
Min RK <benjaminrk@gmail.com>
2018-05-31 16:03:03 +0200

    begin testing DockerSpawner

---
+++ tests/conftest.py
@@ -0,0 +1,42 @@
+"""pytest config for dockerspawner tests"""
+
+from unittest import mock
+
+from docker import from_env as docker_from_env
+import pytest
+
+from jupyterhub.tests.mocking import MockHub
+
+# import base jupyterhub fixtures
+from jupyterhub.tests.conftest import app, io_loop  # noqa
+from dockerspawner import DockerSpawner
+
+# make Hub connectable from docker by default
+MockHub.hub_ip = "0.0.0.0"
+
+
+@pytest.fixture
+def dockerspawner(app):
+    """Configure JupyterHub to use DockerSpawner"""
+    app.config.DockerSpawner.prefix = "dockerspawner-test"
+    # app.config.DockerSpawner.remove = True
+    with mock.patch.dict(app.tornado_settings, {"spawner_class": DockerSpawner}):
+        yield
+
+
+@pytest.fixture(autouse=True, scope="session")
+def docker():
+    """Fixture to return a connected docker client
+
+    cleans up any containers we leave in docker
+    """
+    d = docker_from_env()
+    try:
+        yield d
+
+    finally:
+        # cleanup our containers
+        for c in d.containers.list(all=True):
+            if c.name.startswith("dockerspawner-test"):
+                c.stop()
+                c.remove()
--- tests/volumes_test.py
+++ tests/volumes_test.py
@@ -9,55 +9,68 @@ from traitlets.config import LoggingConfigurable
 
 def test_binds(monkeypatch):
     import jupyterhub
+
     monkeypatch.setattr("jupyterhub.spawner.Spawner", _MockSpawner)
     from dockerspawner.dockerspawner import DockerSpawner
+
     d = DockerSpawner()
-    d.user = types.SimpleNamespace(name='xyz')
-    d.volumes = {
-        'a': 'b',
-        'c': {'bind': 'd', 'mode': 'Z'},
-    }
-    assert d.volume_binds == {
-        'a': {'bind': 'b', 'mode': 'rw'},
-        'c': {'bind': 'd', 'mode': 'Z'},
-    }
-    d.volumes = {'a': 'b', 'c': 'd', 'e': 'f'}
-    assert d.volume_mount_points == ['b', 'd', 'f']
-    d.volumes = {'/nfs/{username}': {'bind': '/home/{username}', 'mode': 'z'}}
-    assert d.volume_binds == {'/nfs/xyz': {'bind': '/home/xyz', 'mode': 'z'}}
-    assert d.volume_mount_points == ['/home/xyz']
+    d.user = types.SimpleNamespace(name="xyz")
+    d.volumes = {"a": "b", "c": {"bind": "d", "mode": "Z"}}
+    assert (
+        d.volume_binds
+        == {"a": {"bind": "b", "mode": "rw"}, "c": {"bind": "d", "mode": "Z"}}
+    )
+    d.volumes = {"a": "b", "c": "d", "e": "f"}
+    assert d.volume_mount_points == ["b", "d", "f"]
+    d.volumes = {"/nfs/{username}": {"bind": "/home/{username}", "mode": "z"}}
+    assert d.volume_binds == {"/nfs/xyz": {"bind": "/home/xyz", "mode": "z"}}
+    assert d.volume_mount_points == ["/home/xyz"]
+
 
 def test_volume_naming_configuration(monkeypatch):
     from dockerspawner.dockerspawner import DockerSpawner
+
     d = DockerSpawner()
-    d.user = types.SimpleNamespace(name='joe')
-    d.volumes = {'data/{username}': {'bind': '/home/{username}', 'mode': 'z'}}
+    d.user = types.SimpleNamespace(name="joe")
+    d.volumes = {"data/{username}": {"bind": "/home/{username}", "mode": "z"}}
 
     def test_format(label_template, spawner):
         return "THIS IS A TEST"
 
     d.format_volume_name = test_format
-    assert d.volume_binds == {'THIS IS A TEST':{'bind': 'THIS IS A TEST', 'mode': 'z'}}
-    assert d.volume_mount_points == ['THIS IS A TEST']
+    assert d.volume_binds == {"THIS IS A TEST": {"bind": "THIS IS A TEST", "mode": "z"}}
+    assert d.volume_mount_points == ["THIS IS A TEST"]
+
 
 def test_default_format_volume_name(monkeypatch):
     from dockerspawner.dockerspawner import DockerSpawner
+
     d = DockerSpawner()
-    d.user = types.SimpleNamespace(name='user@email.com')
-    d.volumes = {'data/{username}': {'bind': '/home/{username}', 'mode': 'z'}}
-    assert d.volume_binds == {'data/user@email.com':{'bind': '/home/user@email.com', 'mode': 'z'}}
-    assert d.volume_mount_points == ['/home/user@email.com']
+    d.user = types.SimpleNamespace(name="user@email.com")
+    d.volumes = {"data/{username}": {"bind": "/home/{username}", "mode": "z"}}
+    assert (
+        d.volume_binds
+        == {"data/user@email.com": {"bind": "/home/user@email.com", "mode": "z"}}
+    )
+    assert d.volume_mount_points == ["/home/user@email.com"]
+
 
 def test_escaped_format_volume_name(monkeypatch):
     import dockerspawner
     from dockerspawner import DockerSpawner
 
     d = DockerSpawner()
-    d.user = types.SimpleNamespace(name='user@email.com')
-    d.volumes = {'data/{username}': {'bind': '/home/{username}', 'mode': 'z'}}
+    d.user = types.SimpleNamespace(name="user@email.com")
+    d.volumes = {"data/{username}": {"bind": "/home/{username}", "mode": "z"}}
     d.format_volume_name = dockerspawner.volumenamingstrategy.escaped_format_volume_name
-    assert d.volume_binds == {'data/user_40email_2Ecom':{'bind': '/home/user_40email_2Ecom', 'mode': 'z'}}
-    assert d.volume_mount_points == ['/home/user_40email_2Ecom']
+    assert (
+        d.volume_binds
+        == {
+            "data/user_40email_2Ecom": {"bind": "/home/user_40email_2Ecom", "mode": "z"}
+        }
+    )
+    assert d.volume_mount_points == ["/home/user_40email_2Ecom"]
+
 
 class _MockSpawner(LoggingConfigurable):
     pass
