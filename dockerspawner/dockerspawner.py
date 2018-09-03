852f5b93621d3295cf4bd9d905b4583bd586df95
Min RK <benjaminrk@gmail.com>
2018-09-03 17:53:30 +0200

    add DockerSpawner.pull_policy
    default: ifnotpresent
    
    This removes the common requirement to pull the default image.
    It is pulled by default on the first launch.

--- dockerspawner/dockerspawner.py
+++ dockerspawner/dockerspawner.py
@@ -20,6 +20,7 @@ from jupyterhub.spawner import Spawner
 from traitlets import (
     Any,
     Bool,
+    CaselessStrEnum,
     Dict,
     List,
     Int,
@@ -265,6 +266,20 @@ class DockerSpawner(Spawner):
         if 'image' in formdata:
             options['image'] = formdata['image'][0]
         return options
+
+    pull_policy = CaselessStrEnum(
+        ["always", "ifnotpresent", "never"],
+        default_value="ifnotpresent",
+        config=True,
+        help="""The policy for pulling the user docker image.
+
+        Choices:
+
+        - ifnotpresent: pull if the image is not already present (default)
+        - always: always pull the image to check for updates, even if it is present
+        - never: never perform a pull
+        """
+    )
 
     container_prefix = Unicode(config=True, help="DEPRECATED in 0.10. Use prefix")
 
@@ -759,6 +774,41 @@ class DockerSpawner(Spawner):
         """
         return self.docker("stop", self.container_id)
 
+
+    @gen.coroutine
+    def pull_image(self, image):
+        """Pull the image, if needed
+
+        - pulls it unconditionally if pull_policy == 'always'
+        - otherwise, checks if it exists, and
+          - raises if pull_policy == 'never'
+          - pulls if pull_policy == 'ifnotpresent'
+        """
+        # docker wants to split repo:tag
+        if ':' in image:
+            repo, tag = image.split(':', 1)
+        else:
+            repo = image
+            tag = 'latest'
+
+        if self.pull_policy.lower() == 'always':
+            # always pull
+            self.log.info("pulling %s", image)
+            yield self.docker('pull', repo, tag)
+            # done
+            return
+        try:
+            # check if the image is present
+            _image = yield self.docker('inspect_image', image)
+        except docker.errors.NotFound:
+            if self.pull_policy == "never":
+                # never pull, raise because there is no such image
+                raise
+            elif self.pull_policy == "ifnotpresent":
+                # not present, pull it for the first time
+                self.log.info("pulling image %s", image)
+                yield self.docker('pull', repo, tag)
+
     @gen.coroutine
     def start(self, image=None, extra_create_kwargs=None, extra_host_config=None):
         """Start the single-user server in a docker container.
@@ -786,6 +836,7 @@ class DockerSpawner(Spawner):
             self.extra_host_config.update(extra_host_config)
 
         image = self.image
+        yield self.pull_image(image)
 
         obj = yield self.get_object()
         if obj and self.remove:
--- tests/test_dockerspawner.py
+++ tests/test_dockerspawner.py
@@ -2,8 +2,8 @@
 
 import json
 
+import docker
 import pytest
-
 from jupyterhub.tests.test_api import add_user, api_request
 from jupyterhub.tests.mocking import public_url
 from jupyterhub.tests.utils import async_requests
@@ -68,3 +68,54 @@ def test_image_whitelist(app, image):
         app, "users", name, "server", method="delete",
     )
     r.raise_for_status()
+
+
+@pytest.mark.gen_test
+def test_image_pull_policy(app):
+    name = "gumby"
+    add_user(app.db, app, name=name)
+    user = app.users[name]
+    assert isinstance(user.spawner, DockerSpawner)
+    spawner = user.spawners[""]
+    spawner.image = "jupyterhub/doesntexist:nosuchtag"
+    with pytest.raises(docker.errors.NotFound):
+        spawner.image_pull_policy = "never"
+        yield spawner.pull_image(spawner.image)
+
+    repo = "busybox"
+    tag = "1.29.1"  # a version that's definitely not latest
+    # ensure image isn't present
+    try:
+        yield spawner.docker("remove_image", "{}:{}".format(repo, tag))
+    except docker.errors.ImageNotFound:
+        raise
+
+    spawner.pull_policy = "ifnotpresent"
+    image = "{}:{}".format(repo, tag)
+    # should trigger a pull
+    yield spawner.pull_image(image)
+    # verify that the image exists now
+    old_image_info = yield spawner.docker("inspect_image", image)
+    print(old_image_info)
+
+    # now tag busybox:latest as our current version
+    # which is not latest!
+    yield spawner.docker("tag", image, repo)
+
+    image = repo  # implicit :latest
+    spawner.pull_policy = "ifnotpresent"
+    # check with ifnotpresent shouldn't pull
+    yield spawner.pull_image(image)
+    image_info = yield spawner.docker("inspect_image", repo)
+    assert image_info["Id"] == old_image_info["Id"]
+
+    # run again with Always,
+    # should trigger a pull even though the image is present
+    spawner.pull_policy = "always"
+    spawner.pull_image(image)
+    image_info = yield spawner.docker("inspect_image", repo)
+    assert image_info["Id"] != old_image_info["Id"]
+
+    # run again with never, make sure it's still happy
+    spawner.pull_policy = "never"
+    spawner.pull_image(image)
