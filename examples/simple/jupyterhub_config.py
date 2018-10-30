d514e4568b6457bd30925eb5589154a1c7f574f9
Min RK <benjaminrk@gmail.com>
2018-10-30 15:38:06 +0100

    add simple example
    shows the basics of networking to get started

---
+++ examples/simple/jupyterhub_config.py
@@ -0,0 +1,22 @@
+
+# dummy for testing. Don't use this in production!
+c.JupyterHub.authenticator_class = 'dummyauthenticator.DummyAuthenticator'
+
+# launch with docker
+c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
+
+# we need the hub to listen on all ips when it is in a container
+c.JupyterHub.hub_ip = '0.0.0.0'
+# the hostname/ip that should be used to connect to the hub
+# this is usually the hub container's name
+c.JupyterHub.hub_connect_ip = 'jupyterhub'
+
+# pick a docker image. This should have the same version of jupyterhub
+# in it as our Hub.
+c.DockerSpawner.image = 'jupyter/base-notebook'
+
+# tell the user containers to connect to our docker network
+c.DockerSpawner.network_name = 'jupyterhub'
+
+# delete containers when the stop
+c.DockerSpawner.remove = True
