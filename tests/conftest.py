ae55e69fb265379e6506074c9b3ca776ea7f1a95
GeorgianaElena <georgiana.dolocan@gmail.com>
2020-06-23 17:18:35 +0300

    Replace pytest-tornado with pytest-asyncio

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,20 +9,41 @@ import pytest
 from jupyterhub.tests.mocking import MockHub
 
 # import base jupyterhub fixtures
-from jupyterhub.tests.conftest import app, io_loop  # noqa
-from dockerspawner import DockerSpawner
+from jupyterhub.tests.conftest import app, io_loop, event_loop, ssl_tmpdir  # noqa
+from dockerspawner import DockerSpawner, SwarmSpawner, SystemUserSpawner
 
 # make Hub connectable from docker by default
 MockHub.hub_ip = "0.0.0.0"
 
 
 @pytest.fixture
-def dockerspawner(app):
+def dockerspawner_configured_app(app):
     """Configure JupyterHub to use DockerSpawner"""
     app.config.DockerSpawner.prefix = "dockerspawner-test"
     # app.config.DockerSpawner.remove = True
     with mock.patch.dict(app.tornado_settings, {"spawner_class": DockerSpawner}):
-        yield
+        yield app
+
+@pytest.fixture
+def swarmspawner_configured_app(app):
+    """Configure JupyterHub to use DockerSpawner"""
+    app.config.SwarmSpawner.prefix = "dockerspawner-test"
+    with mock.patch.dict(
+        app.tornado_settings, {"spawner_class": SwarmSpawner}
+    ), mock.patch.dict(
+        app.config.SwarmSpawner, {"network_name": "bridge"}
+    ):
+        yield app
+
+
+@pytest.fixture
+def systemuserspawner_configured_app(app):
+    """Configure JupyterHub to use DockerSpawner"""
+    app.config.SwarmSpawner.prefix = "dockerspawner-test"
+    with mock.patch.dict(
+        app.tornado_settings, {"spawner_class": SystemUserSpawner}
+    ):
+        yield app
 
 
 @pytest.fixture(autouse=True, scope="session")
