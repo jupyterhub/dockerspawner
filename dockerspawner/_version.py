c749ff0a61e9b5387707f4b43db1b62f275ec2f8
Min RK <benjaminrk@gmail.com>
2021-07-22 12:13:53 +0200

    12.1.0

--- dockerspawner/_version.py
+++ dockerspawner/_version.py
@@ -6,7 +6,7 @@ version_info = (
     12,
     1,
     0,
-    'dev',  # comment-out this line for a release
+    # 'dev',  # comment-out this line for a release
 )
 __version__ = '.'.join(map(str, version_info[:3]))
 
