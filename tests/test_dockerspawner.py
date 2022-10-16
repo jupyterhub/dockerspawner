0012138ff9fcb949c5f0b55830cb3ced5b585572
Sheila Kahwai <sheilankw@gmail.com>
2022-10-16 10:28:45 +0300

    add tests for allowed images and default host ip

--- tests/test_dockerspawner.py
+++ tests/test_dockerspawner.py
@@ -18,7 +18,6 @@ from dockerspawner import DockerSpawner
 # Mark all tests in this file as asyncio
 pytestmark = pytest.mark.asyncio
 
-
 def test_name_collision(dockerspawner_configured_app):
     app = dockerspawner_configured_app
     has_hyphen = "user--foo"
@@ -81,28 +80,45 @@ async def test_start_stop(dockerspawner_configured_app, remove):
     else:
         assert sorted(state) == ["object_id", "object_name"]
 
+def allowed_images_callable(*_):
+    return ["jupyterhub/singleuser:1.0", "jupyterhub/singleuser:1.1"]
 
-@pytest.mark.parametrize("image", ["1.0", "1.1.0", "nomatch"])
-async def test_allowed_image(dockerspawner_configured_app, image):
+@pytest.mark.parametrize(
+    "image, expected",
+    [
+    (
+    {
+        "1.0": "jupyterhub/singleuser:1.0",
+        "1.1": "jupyterhub/singleuser:1.1",
+    },
+    "1.0"
+    ),
+    (
+    ["jupyterhub/singleuser:1.0", "jupyterhub/singleuser:1.1.0"],
+    "1.1.0"
+    ),
+    (allowed_images_callable, "1.0"),
+    ],
+    )
+async def test_allowed_image(dockerspawner_configured_app, image, expected):
     app = dockerspawner_configured_app
     name = "checker"
     add_user(app.db, app, name=name)
     user = app.users[name]
     assert isinstance(user.spawner, DockerSpawner)
     user.spawner.remove_containers = True
-    user.spawner.allowed_images = {
-        "1.0": "jupyterhub/singleuser:1.0",
-        "1.1": "jupyterhub/singleuser:1.1",
-    }
+    user.spawner.allowed_images = image
     token = user.new_api_token()
     # start the server
     r = await api_request(
-        app, "users", name, "server", method="post", data=json.dumps({"image": image})
+        app, "users", name, "server", method="post", data=json.dumps({"image": expected})
     )
-    if image not in user.spawner.allowed_images:
-        with pytest.raises(Exception):
-            r.raise_for_status()
-        return
+
+    if not callable(image):
+        if expected not in user.spawner.allowed_images:
+            with pytest.raises(Exception):
+                r.raise_for_status()
+            return
     pending = r.status_code == 202
     while pending:
         # request again
@@ -117,7 +133,7 @@ async def test_allowed_image(dockerspawner_configured_app, image):
     assert resp.effective_url == url
     resp.rethrow()
 
-    assert resp.headers['x-jupyterhub-version'].startswith(image)
+    assert resp.headers['x-jupyterhub-version'].startswith(expected)
     r = await api_request(
         app,
         "users",
@@ -298,3 +314,13 @@ async def test_cpu_limit(dockerspawner_configured_app, cpu_limit, expected, user
     else:
         assert 'CpuPeriod' not in host_config
         assert 'CpuQuota' not in host_config
+
+@mock.patch.dict(os.environ, {"DOCKER_HOST": "tcp://127.0.0.2"}, clear=True)
+def test_default_host_ip_reads_env_var(dockerspawner_configured_app):
+    app = dockerspawner_configured_app
+    name = "marshmellow"
+    add_user(app.db, app, name=name)
+    user = app.users[name]
+    assert isinstance(user.spawner, DockerSpawner)
+    spawner = user.spawners[""]
+    assert spawner._default_host_ip() == "127.0.0.2"
