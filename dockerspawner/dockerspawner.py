1f73a9f6adc5fed2f933d0c7d1c949348497308b
YuviPanda <yuvipanda@gmail.com>
2025-04-04 12:47:41 -0700

    Unpack list of dicts correctly

--- dockerspawner/dockerspawner.py
+++ dockerspawner/dockerspawner.py
@@ -1169,8 +1169,8 @@ class DockerSpawner(Spawner):
 
         # Make a list of mount objects if present
         mount_binds = [Mount(
-            **{k: self.format_volume_name(v, self) for k, v in self.mounts.items()}
-        )]
+            **{k: self.format_volume_name(v, self) for k, v in mount.items()}
+        ) for mount in self.mounts]
 
         # build the dictionary of keyword arguments for host_config
 
